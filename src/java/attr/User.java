5fcb977035a9281eaec32fab6f76a853d16186d4
RajithaKumara <tmrkumara.15@cse.mrt.ac.lk>
2017-11-08 19:39:03 +0530

    Revert "added putmarks logic code part 1"
    This reverts commit 6ec47bc66b1d41db98ef074daf667fb821444432.

--- src/java/attr/User.java
+++ src/java/attr/User.java
@@ -17,7 +17,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.management.Query;
 
 public class User {
     private String user_name=null;
@@ -25,9 +24,6 @@ public class User {
     private String password=null;
     private String telephone=null;
     private String email=null;
-    
-   
-  
     
     public ResultSet Login(String uname , String password) throws IOException{
         ResultSet rs = null;
@@ -40,8 +36,8 @@ public class User {
             stmt = con.createStatement();
             //rs = stmt.executeQuery("select user_name,password,occupation from interviewer where user_name = '"+uname+"' and password = '"+password+"'");
             rs = stmt.executeQuery("select user_name,pass,role_name from user join ((select user_id,role_name from user_role join role on user_role.role_id=role.role_id) as r) using (user_id) where user_name='"+uname+"' and pass='"+password+"'");
-//            select * from user join ((select user_id,role_name from user_role join role on user_role.role_id=role.role_id) as r) using (user_id) where user_name='rajitha';
             
+
         } catch (SQLException ex) {  
         }
         
@@ -57,9 +53,8 @@ public class User {
             Statement stmt;
           
             stmt = con.createStatement();
-            rs = stmt.executeQuery("select * from interviewer where user_name = '"+uname+"'");
-            
-            
+            rs = stmt.executeQuery("select * from user where user_name = '"+uname+"'");
+  
         
         } catch (SQLException ex) {   
         }
@@ -67,25 +62,84 @@ public class User {
         return rs;
     }
     
-    public boolean registerUser(String user_name,String occupation,String tel,String pass) throws IOException{
-        ResultSet rs = null;
+    
+    public boolean changeUserPassword(String user_name,String new_password) throws IOException{
         boolean success=false;
+        //UPDATE user SET `pass = '*A4B6157319038724E3560894F7F932C8886EBFCF' WHERE `interviewer`.`user_id` = 3;
         try {
             DbConnector db = new DbConnector();
             Connection con =db.getCon();
-            Statement stmt;
           
-            stmt = con.createStatement();
             
-            stmt.executeUpdate("INSERT INTO interviewer (user_name,password,telephone,occupation) VALUES ('"+user_name+"', password('"+pass+"'), '"+tel+"', '"+occupation+"')");
+            Statement stmt = con.createStatement();
+            stmt.executeUpdate("UPDATE user SET pass = '"+new_password+"' WHERE user_name='"+user_name+"'");
+            
             success=true;
             
-        
         } catch (SQLException ex) {  
         }
         
         return success;
     }
+    
+    public boolean registerUser(String user_name,String occupation,String tel,String pass) throws IOException{
+
+        boolean success=false;
+        try {
+            DbConnector db = new DbConnector();
+            Connection con =db.getCon();
+          
+            Statement stmt1 = con.createStatement();
+            stmt1.executeUpdate("INSERT INTO user (user_name,pass,telephone) VALUES ('"+user_name+"', password('"+pass+"'), '"+tel+"')");
+            
+            Statement stmt2 = con.createStatement();
+            ResultSet rs1 = stmt2.executeQuery("select user_id from user where user_name = '"+user_name+"'");
+            
+            String user_id=null;
+            if(rs1.next()){
+                user_id=(String)rs1.getString("user_id");
+            }
+            
+            Statement stmt3 = con.createStatement();
+            ResultSet rs2 = stmt3.executeQuery("select role_id from role where role_name = '"+occupation+"'");
+            String role_id=null;
+            if(rs2.next()){
+                role_id=(String)rs2.getString("role_id");
+            }
+            
+            
+            Statement stmt4 = con.createStatement();
+            stmt4.executeUpdate("INSERT INTO user_role (user_id,role_id) VALUES ('"+user_id+"', '"+role_id+"')");
+            
+            success=true;
+        
+        } catch (SQLException ex) { 
+            
+        }
+        
+        return success;
+    }
+    
+    
+    public boolean deleteUser(String user_name) throws IOException{
+       
+        boolean success=false;
+        try {
+            DbConnector db = new DbConnector();
+            Connection con =db.getCon();
+          
+            
+            Statement stmt = con.createStatement();
+            stmt.executeUpdate("delete from user where user_name='"+user_name+"'");
+            
+            success=true;
+            
+        } catch (SQLException ex) {  
+        }
+        
+        return success;
+    }
+    
     
     public String returnEncryptedPassword(String password) throws IOException{
         ResultSet rs = null;
@@ -106,26 +160,6 @@ public class User {
         } catch (SQLException ex) {  
         }
         return encrypted_password;
-    }
-    public String putMarks() throws IOException{
-       ResultSet rs = null;
-        
-        try {
-            DbConnector db = new DbConnector();
-            Connection con =db.getCon();
-            Statement stmt;
-          
-            stmt = con.createStatement();
-            //rs = stmt.executeQuery("select * from interview");
-            stmt.executeUpdate("insert into interview values('1','1','12','2017-11-18')");
-            
-        
-        } catch (SQLException ex) {   
-            return ex.toString();
-        }
-        
-        
-     return "success";   
     }
     
 }
