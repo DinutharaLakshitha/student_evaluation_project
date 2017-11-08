f0ce01f1d4205e876d165c0ed17df7fa0ac49401
Rajitha Kumara <tmrkumara.15@cse.mrt.ac.lk>
2017-11-09 03:52:28 +0530

    User password change validation ok

--- src/java/serv/ChangePassword.java
+++ src/java/serv/ChangePassword.java
@@ -6,10 +6,12 @@
 package serv;
 
 import attr.User;
+import attr.Validation;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
@@ -41,6 +43,10 @@ public class ChangePassword extends HttpServlet {
         try (PrintWriter out = response.getWriter()) {
 //            UPDATE `interviewer` SET `password` = '*A4B6157319038724E3560894F7F932C8886EBFCF' WHERE `interviewer`.`user_id` = 3;
             HttpSession session=request.getSession(); 
+            ArrayList<String> error;
+            error = new ArrayList<>();
+            session.setAttribute("Error", error);
+
 
             if(session.getAttribute("uname")==null){
                 response.sendRedirect("index.jsp");
@@ -55,43 +61,43 @@ public class ChangePassword extends HttpServlet {
                 if (current_password==null){
                     response.sendRedirect("changePassword.jsp");
                 }else{
-
-                    if (new_password.equals(confirm_password)){
-                        User u =new User();
+//                    error.add("Username already in use.");
+                    User u =new User();
                         
-                        String encrypted_password=u.returnEncryptedPassword(current_password);
-                        String encrypted_new_password=u.returnEncryptedPassword(new_password);
-
-                        out.println(user_name);
-                        out.println(current_password);
-                        out.println(new_password);
-                        out.println(confirm_password);
-
-                        ResultSet rs = u.getDetails(user_name);
+                    String encrypted_password=u.returnEncryptedPassword(current_password);
+                    String encrypted_new_password=u.returnEncryptedPassword(new_password);
+                    
+                    ResultSet rs = u.getDetails(user_name);
                         
-                        if(rs.next()){
-                            String old_password=(String) rs.getString("pass");
-                            if (old_password.equals(encrypted_password)){
-                                boolean success=u.changeUserPassword(user_name, encrypted_new_password);
-                                if (success){
-                                    session.setAttribute("success","Password successfully changed.");
-                                }else{
-                                    session.setAttribute("error","Erorr while changing password.");
-                                }
-                                
-                                response.sendRedirect("changePassword.jsp");
-                            }
-                            else{
-                                session.setAttribute("error","Wrong password.Try again.");
-                                response.sendRedirect("changePassword.jsp");
-                            }
+                    if(rs.next()){
+                        String old_password=(String) rs.getString("pass");
+                        if (!old_password.equals(encrypted_password)){
+                            error.add("Wrong password.Try again.");
                         }
                     }
-                    else{
-                        
-                        session.setAttribute("error","New password not match");
+                    Validation validation=new Validation();
+            
+                    if (validation.validatePasswordStrong(new_password)){
+                        if (!new_password.equals(confirm_password)){
+                            error.add("New password not match");
+                        }
+                    }else{
+                        error.add("New password should have Capital letters, signs, numbers.");
+                    }
+                    
+                    
+                    if (error.size()>0){
+                        response.sendRedirect("changePassword.jsp");
+                    }else{
+                        boolean success=u.changeUserPassword(user_name, encrypted_new_password);
+                        if (success){
+                            session.setAttribute("success","Password successfully changed.");
+                        }else{
+                            error.add("Erorr while changing password.");
+                        }
                         response.sendRedirect("changePassword.jsp");
                     }
+//                        
                 }
 
             }
