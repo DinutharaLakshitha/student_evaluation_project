ae68fd7a04bbee74bf930ba661cf37cb1cbcf553
DinutharaLakshitha <dinutharalakshitha@gmail.com>
2017-11-08 18:34:03 +0530

    Updated validations

--- src/java/attr/Validation.java
+++ src/java/attr/Validation.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -5,10 +6,60 @@
  */
 package attr;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author Dinuthara
  */
 public class Validation {
+    
+    public boolean onlyLettersSpaces(String s){
+  for(int i=0;i<s.length();i++){
+    char ch = s.charAt(i);
+    if (Character.isLetter(ch) || ch == ' ') {
+      continue;
+    }
+    return false;
+  }
+  return true;
+}
+    
+ public  boolean addressValidation(String address) {
+String regex;
+regex = "^[a-zA-Z0-9.-/s]+$";
+String pattern = null;
+Pattern r = Pattern.compile(pattern); 
+Matcher m = r.matcher(address);
+ 
+if(m.find()) {
+ return true;
+} else {
+ return false;
+}
+}
+ 
+     public boolean onlyLettersSpacesdot(String s){
+  for(int i=0;i<s.length();i++){
+    char ch = s.charAt(i);
+    if (Character.isLetter(ch) || ch == ' ' || ch =='.') {
+      continue;
+    }
+    return false;
+  }
+  return true;
+}
+     
+          public boolean onlyInitials(String s){
+  for(int i=0;i<s.length();i++){
+    char ch = s.charAt(i);
+    if (Character.isUpperCase(ch) || ch =='.') {
+      continue;
+    }
+    return false;
+  }
+  return true;
+}
     
 }
