6bc8a7e852e925f3eb92a7d85cf50dd44dae1e56
DinutharaLakshitha <dinutharalakshitha@gmail.com>
2017-11-08 15:33:33 +0530

    add Applicant servelet

---
+++ src/java/serv/addApplicant.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serv;
+
+import attr.Applicant;
+import java.awt.List;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Dinuthara
+ */
+@WebServlet(name = "addApplicant", urlPatterns = {"/addApplicant"})
+public class addApplicant extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     * @throws java.sql.SQLException
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            ArrayList<String> error;
+            error = new ArrayList<>();
+            HttpSession session=request.getSession();
+            session.setAttribute("Error", error);
+            
+            
+            
+         
+            String f_name = request.getParameter("txt_fname");
+            String initial = request.getParameter("txt_init");
+            String l_name = request.getParameter("txt_lname");
+       
+            String h_num = request.getParameter("txt_home_num");
+            String street_name = request.getParameter("txt_street_name");
+            String city_name = request.getParameter("txt_city_name");
+            String district = request.getParameter("txt_district");
+            String grama = request.getParameter("txt_grama");
+            
+            
+            String b_day = request.getParameter("date");
+            String b_month = request.getParameter("month");
+            
+            String dob = "0000-00-00";
+            
+            String gender = request.getParameter("gender");
+            String religeon = request.getParameter("religion");
+            
+            
+             
+             /*
+                Applicant applicant = new Applicant();
+            
+                out.println("Object created");
+            
+                boolean a = applicant.register(f_name, l_name,initial, gender, dob, h_num, street_name, city_name,district,grama,gender,religeon);
+                out.println(a);
+            }*/
+            
+            
+            
+            
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(addApplicant.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(addApplicant.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
