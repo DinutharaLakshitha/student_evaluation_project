cbbc08128c0d0c0b63f03dbd0a1e626246d30303
chathuranga95 <chathuranga.15@cse.mrt.ac.lk>
2017-12-09 23:23:14 +0530

    School class SQL statements updated with prepared statements

--- src/java/attr/School.java
+++ src/java/attr/School.java
@@ -7,6 +7,7 @@ package attr;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -19,31 +20,28 @@ import utill.DbConnector;
  * @author Dinuthara
  */
 public class School {
-    
-        public boolean register(String name,String division,String district,String provision,String count) throws IOException, SQLException{
+
+    public boolean register(String name, String division, String district, String provision, String count) throws IOException, SQLException {
         ResultSet rs = null;
-        boolean success=false;
+        boolean success = false;
         try {
             DbConnector db = new DbConnector();
-            Connection conn =db.getCon();
-             Statement stmt;
-          
-            stmt = conn.createStatement();
-            stmt.executeUpdate("INSERT INTO school (`name`, `division`, `district`, `provision`, `eligible_count`) VALUES ('"+name+"', '"+division+"', '"+district+"', '"+provision+"', '"+count+"')");
-            /*stmt.executeUpdate("INSERT INTO applicant (`dob`) VALUES ('"+dob+"')");*/
-            success=true;
-         
-            
-            
-        
+            Connection conn = db.getCon();
+
+            String rawString = "INSERT INTO school (`name`, `division`, `district`, `provision`, `eligible_count`) VALUES (?,?,?,?,?)";
+            PreparedStatement statement = conn.prepareStatement(rawString);
+            statement.setString(1, name);
+            statement.setString(2, division);
+            statement.setString(3, district);
+            statement.setString(4, provision);
+            statement.setString(5, count);
+            statement.executeUpdate();
+
+            success = true;
+
         } catch (SQLException ex) {
             Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
-            
-            
         }
-        
         return success;
-    
-}
-    
+    }
 }
