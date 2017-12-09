e3739769778a673d9e9d676bd1f94c058905d529
chathuranga95 <chathuranga.15@cse.mrt.ac.lk>
2017-12-09 22:44:28 +0530

    User class SQL statements updated with prepared statements

--- src/java/attr/User.java
+++ src/java/attr/User.java
@@ -12,6 +12,7 @@ package attr;
 import java.io.IOException;
 import utill.DbConnector;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -21,155 +22,169 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class User {
-    private String user_name=null;
-    private String ocupation=null;
-    private String password=null;
-    private String telephone=null;
-    private String email=null;
-    
-    public ResultSet Login(String uname , String password) throws IOException{
-        ResultSet rs = null;
-        
-        try {
-            DbConnector db = new DbConnector();
-            Connection con =db.getCon();
-            Statement stmt;
-          
-            stmt = con.createStatement();
-            //SQL injection to be implemented
-            //rs = stmt.executeQuery("select user_name,password,occupation from interviewer where user_name = '"+uname+"' and password = '"+password+"'");
-            rs = stmt.executeQuery("select user_name,pass,role_name from user join ((select user_id,role_name from user_role join role on user_role.role_id=role.role_id) as r) using (user_id) where user_name='"+uname+"' and pass='"+password+"'");
-            
 
-        } catch (SQLException ex) {  
-        }
-        
-        return rs;
-    }
-    
-    public ResultSet getDetails(String uname) throws IOException{
+    private String user_name = null;
+    private String ocupation = null;
+    private String password = null;
+    private String telephone = null;
+    private String email = null;
+
+    public ResultSet Login(String uname, String password) throws IOException {
         ResultSet rs = null;
-        
+
         try {
             DbConnector db = new DbConnector();
-            Connection con =db.getCon();
-            Statement stmt;
-          
-            stmt = con.createStatement();
-            rs = stmt.executeQuery("select * from user where user_name = '"+uname+"'");
-  
-        
-        } catch (SQLException ex) {   
+            Connection con = db.getCon();
+
+            String rawString = "select user_name,pass,role_name from user join ((select user_id,role_name from user_role join role on user_role.role_id=role.role_id) as r) using (user_id) where user_name=? and pass=?";
+            PreparedStatement statement = con.prepareStatement(rawString);
+            statement.setString(1, uname);
+            statement.setString(2, password);
+            rs = statement.executeQuery();
+
+        } catch (SQLException ex) {
         }
-        
+
         return rs;
     }
-    
-    
-    public boolean changeUserPassword(String user_name,String new_password) throws IOException{
-        boolean success=false;
+
+    public ResultSet getDetails(String uname) throws IOException {
+        ResultSet rs = null;
+
+        try {
+            DbConnector db = new DbConnector();
+            Connection con = db.getCon();
+
+            String rawString = "select * from user where user_name = ?";
+            PreparedStatement statement = con.prepareStatement(rawString);
+            statement.setString(1, uname);
+            rs = statement.executeQuery();
+
+        } catch (SQLException ex) {
+        }
+
+        return rs;
+    }
+
+    public boolean changeUserPassword(String user_name, String new_password) throws IOException {
+        boolean success = false;
         //UPDATE user SET `pass = '*A4B6157319038724E3560894F7F932C8886EBFCF' WHERE `interviewer`.`user_id` = 3;
         try {
             DbConnector db = new DbConnector();
-            Connection con =db.getCon();
-          
-            
-            Statement stmt = con.createStatement();
-            stmt.executeUpdate("UPDATE user SET pass = '"+new_password+"' WHERE user_name='"+user_name+"'");
-            
-            success=true;
-            
-        } catch (SQLException ex) {  
-        }
-        
-        return success;
-    }
-    
-    public boolean registerUser(String user_name,String occupation,String tel,String pass,String email) throws IOException{
+            Connection con = db.getCon();
 
-        boolean success=false;
-        try {
-            DbConnector db = new DbConnector();
-            Connection con =db.getCon();
-          
-            Statement stmt1 = con.createStatement();
-            stmt1.executeUpdate("INSERT INTO user (user_name,pass,telephone,email) VALUES ('"+user_name+"', password('"+pass+"'), '"+tel+"','"+email+"')");
-            
-            Statement stmt2 = con.createStatement();
-            ResultSet rs1 = stmt2.executeQuery("select user_id from user where user_name = '"+user_name+"'");
-            
-            String user_id=null;
-            if(rs1.next()){
-                user_id=(String)rs1.getString("user_id");
-            }
-            
-            Statement stmt3 = con.createStatement();
-            ResultSet rs2 = stmt3.executeQuery("select role_id from role where role_name = '"+occupation+"'");
-            String role_id=null;
-            if(rs2.next()){
-                role_id=(String)rs2.getString("role_id");
-            }
-            
-            
-            Statement stmt4 = con.createStatement();
-            stmt4.executeUpdate("INSERT INTO user_role (user_id,role_id) VALUES ('"+user_id+"', '"+role_id+"')");
-            
-            success=true;
-        
-        } catch (SQLException ex) { 
-            
+            String rawString = "UPDATE user SET pass = ? WHERE user_name = ?";
+            PreparedStatement statement = con.prepareStatement(rawString);
+            statement.setString(1,new_password);
+            statement.setString(2,user_name);
+            statement.executeUpdate();
+
+            success = true;
+
+        } catch (SQLException ex) {
         }
-        
+
         return success;
     }
-    
-    
-    public boolean deleteUser(String user_name) throws IOException{
-       
-        boolean success=false;
+
+    public boolean registerUser(String user_name, String occupation, String tel, String pass, String email) throws IOException {
+
+        boolean success = false;
         try {
             DbConnector db = new DbConnector();
-            Connection con =db.getCon();
-          
+            Connection con = db.getCon();
             
-            Statement stmt = con.createStatement();
-            stmt.executeUpdate("delete from user where user_name='"+user_name+"'");
+            String rawString = "INSERT INTO user (user_name,pass,telephone,email) VALUES(?, password(?), ?,?)";
+            PreparedStatement statement = con.prepareStatement(rawString);
+            statement.setString(1,user_name);
+            statement.setString(2,pass);
+            statement.setString(3,tel);
+            statement.setString(4,email);
+            statement.executeUpdate();
             
-            success=true;
             
-        } catch (SQLException ex) {  
+            String rawString1 = "select user_id from user where user_name = ?";
+            PreparedStatement statement1 = con.prepareStatement(rawString1);
+            statement1.setString(1,user_name);
+            ResultSet rs1 = statement1.executeQuery();
+            
+
+            String user_id = null;
+            if (rs1.next()) {
+                user_id = (String) rs1.getString("user_id");
+            }
+
+            
+            String rawString2 = "select role_id from role where role_name = ?";
+            PreparedStatement statement2 = con.prepareStatement(rawString2);
+            statement2.setString(1,occupation);
+            ResultSet rs2 = statement2.executeQuery();
+            
+            
+            String role_id = null;
+            if (rs2.next()) {
+                role_id = (String) rs2.getString("role_id");
+            }
+
+            String rawString3 = "INSERT INTO user_role (user_id,role_id) VALUES (?,?)";
+            PreparedStatement statement3 = con.prepareStatement(rawString3);
+            statement3.setString(1,user_id);
+            statement3.setString(2,role_id);
+            statement3.executeUpdate();
+            
+            success = true;
+
+        } catch (SQLException ex) {
+
         }
-        
+
         return success;
     }
-    
-    
-    public String returnEncryptedPassword(String password) throws IOException{
+
+    public boolean deleteUser(String user_name) throws IOException {
+
+        boolean success = false;
+        try {
+            DbConnector db = new DbConnector();
+            Connection con = db.getCon();
+
+            String rawString = "delete from user where user_name= ?";
+            PreparedStatement statement = con.prepareStatement(rawString);
+            statement.setString(1,user_name);
+            statement.executeUpdate();
+            
+            success = true;
+
+        } catch (SQLException ex) {
+        }
+
+        return success;
+    }
+
+    public String returnEncryptedPassword(String password) throws IOException {
         ResultSet rs = null;
-        String encrypted_password=null;
-        
+        String encrypted_password = null;
+
         try {
             DbConnector db = new DbConnector();
-            Connection con =db.getCon();
-            Statement stmt;
-          
-            stmt = con.createStatement();
-            rs = stmt.executeQuery("select password('"+password+"') as pass");
+            Connection con = db.getCon();
             
-            if(rs.next()){
-                encrypted_password = (String)rs.getString("pass");
+            String rawString = "select password(?) as pass";
+            PreparedStatement statement = con.prepareStatement(rawString);
+            statement.setString(1,password);
+            rs = statement.executeQuery();
+            
+            if (rs.next()) {
+                encrypted_password = (String) rs.getString("pass");
             }
- 
-        } catch (SQLException ex) {  
+
+        } catch (SQLException ex) {
         }
         return encrypted_password;
     }
-    
-    
 
 //
 //    public String putMarks(String u_id, String appid, int mark) {
 //        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
 //    }
-    
 }
