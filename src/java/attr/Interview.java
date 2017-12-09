fca1506b039e6edcc9f03431f23a381853c83bfa
chathuranga95 <chathuranga.15@cse.mrt.ac.lk>
2017-12-09 23:13:30 +0530

    Interview class SQL statements updated with prepared statements

--- src/java/attr/Interview.java
+++ src/java/attr/Interview.java
@@ -7,11 +7,14 @@ package attr;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import utill.DbConnector;
 
 /**
@@ -26,23 +29,24 @@ public class Interview {
         try {
             DbConnector db = new DbConnector();
             Connection con = db.getCon();
-            Statement stmt;
-
-            stmt = con.createStatement();
-            rs = stmt.executeQuery("select school_id from school where name = '" + name + "'");
+            
+            String rawString = "select school_id from school where name = ?";
+            PreparedStatement statement = con.prepareStatement(rawString);
+            statement.setString(1,name);
+            rs = statement.executeQuery();
+            
         } catch (SQLException ex) {
             school_id = "error";
         }
 
         if (rs.next()) {
             school_id = rs.getString("school_id");
-        }
-        else{
+        } else {
             school_id = "no results";
         }
         return school_id;
     }
-    
+
     public float getDistanceMarks(String applicant_id, String school_id) throws IOException, SQLException {
         ResultSet rs = null;
         float mark = 0;
@@ -50,40 +54,90 @@ public class Interview {
         try {
             DbConnector db = new DbConnector();
             Connection con = db.getCon();
-            Statement stmt;
-
-            stmt = con.createStatement();
-            rs = stmt.executeQuery("select distance from applicant_school where applicant_id = '" + applicant_id + "' and school_id = '"+school_id+"'");
-        } catch (SQLException ex) {
             
+            String rawString = "select distance from applicant_school where applicant_id = ? and school_id = ?";
+            PreparedStatement statement = con.prepareStatement(rawString);
+            statement.setString(1,applicant_id);
+            statement.setString(2,school_id);
+            rs = statement.executeQuery();
+            
+            
+        } catch (SQLException ex) {
+
         }
 
         if (rs.next()) {
             distance = Float.parseFloat(rs.getString("distance"));
         }
-        
+
         //criteria
-        if(distance>20){
+        if (distance > 20) {
             mark = 5;
-        }
-        else if(distance>15){
+        } else if (distance > 15) {
             mark = 8;
-        }
-        else if(distance>10){
+        } else if (distance > 10) {
             mark = 10;
-        }
-        else{
+        } else {
             mark = 13;
         }
-        
+
         return mark;
     }
-public String putMarks(String u_id, String app_id, String school_id, float mark) throws IOException, ParseException {
+
+    public ArrayList<String> getReferences(String applicantID) throws IOException, SQLException {
+        ArrayList<String> refList = new ArrayList<>();
+        ResultSet rs = null;
+        try {
+            DbConnector db = new DbConnector();
+            Connection con = db.getCon();
+            
+            String rawString = "select student_id from applicant_reference_student where applicant_id = ?";
+            PreparedStatement statement = con.prepareStatement(rawString);
+            statement.setString(1,applicantID);
+            rs = statement.executeQuery();
+            
+        } catch (SQLException ex) {
+
+        }
+
+        while (rs.next()) {
+            refList.add(rs.getString("student_id"));
+        }
+        return refList;
+    }
+
+    public ArrayList<String> getRefereeDetails(String student_ID) throws IOException, SQLException {
+        ArrayList<String> details = new ArrayList<>();
+        ResultSet rs = null;
+        try {
+            DbConnector db = new DbConnector();
+            Connection con = db.getCon();
+            
+            String rawString = "select distinct first_name, last_name,grade_5,ol,al,other from student, student_school where student.student_id = ? and student_school.student_id = ? and student.student_id = student_school.student_id";
+            PreparedStatement statement = con.prepareStatement(rawString);
+            statement.setString(1,student_ID);
+            statement.setString(2,student_ID);
+            rs = statement.executeQuery();
+            
+            
+        } catch (SQLException ex) {
+            details.add("error occured");
+        }
+
+        while (rs.next()) {
+            for (int i = 1; i <= 6; i++) {
+                details.add(rs.getString(i));
+            }
+        }
+        return details;
+    }
+
+    public String putMarks(String u_id, String app_id, String school_id, float mark) throws IOException, ParseException {
 
         try {
             DbConnector db = new DbConnector();
             Connection con = db.getCon();
-            Statement stmt;
+            
 
             Date date = new Date();
             int dd = date.getDate();
@@ -91,10 +145,16 @@ public String putMarks(String u_id, String app_id, String school_id, float mark)
             int yy = date.getYear() + 1990;
             String dt = yy + "-" + mm + "-" + dd;
 
-            stmt = con.createStatement();
-            //rs = stmt.executeQuery("select * from interview");
-            stmt.executeUpdate("insert into interview values('" + u_id + "','" + app_id + "','" + school_id + "','" + mark + "','" + dt + "')");
-
+            String rawString = "insert into interview values(?,?,?,?,?)";
+            PreparedStatement statement = con.prepareStatement(rawString);
+            statement.setString(1,u_id);
+            statement.setString(2,app_id);
+            statement.setString(3,school_id);
+            String mk = mark + "";
+            statement.setString(4,mk);
+            statement.setString(5,dt);
+            statement.executeUpdate();
+            
         } catch (SQLException ex) {
             return "error";
         }
