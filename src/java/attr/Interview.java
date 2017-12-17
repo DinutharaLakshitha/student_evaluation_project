51112b76ee96f6dea76d186dd722bffb0c215cf2
chathuranga95 <chathuranga.15@cse.mrt.ac.lk>
2017-12-17 16:54:30 +0530

    inerview class modified

--- src/java/attr/Interview.java
+++ src/java/attr/Interview.java
@@ -6,6 +6,7 @@
 package attr;
 
 import java.io.IOException;
+import static java.lang.System.out;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -29,12 +30,12 @@ public class Interview {
         try {
             DbConnector db = new DbConnector();
             Connection con = db.getCon();
-            
+
             String rawString = "select school_id from school where name = ?";
             PreparedStatement statement = con.prepareStatement(rawString);
-            statement.setString(1,name);
+            statement.setString(1, name);
             rs = statement.executeQuery();
-            
+
         } catch (SQLException ex) {
             school_id = "error";
         }
@@ -54,14 +55,13 @@ public class Interview {
         try {
             DbConnector db = new DbConnector();
             Connection con = db.getCon();
-            
+
             String rawString = "select distance from applicant_school where applicant_id = ? and school_id = ?";
             PreparedStatement statement = con.prepareStatement(rawString);
-            statement.setString(1,applicant_id);
-            statement.setString(2,school_id);
+            statement.setString(1, applicant_id);
+            statement.setString(2, school_id);
             rs = statement.executeQuery();
-            
-            
+
         } catch (SQLException ex) {
 
         }
@@ -90,12 +90,12 @@ public class Interview {
         try {
             DbConnector db = new DbConnector();
             Connection con = db.getCon();
-            
+
             String rawString = "select student_id from applicant_reference_student where applicant_id = ?";
             PreparedStatement statement = con.prepareStatement(rawString);
-            statement.setString(1,applicantID);
+            statement.setString(1, applicantID);
             rs = statement.executeQuery();
-            
+
         } catch (SQLException ex) {
 
         }
@@ -105,6 +105,50 @@ public class Interview {
         }
         return refList;
     }
+    
+    public String getSchoolName(String school_id) throws IOException, SQLException {
+        String  name = "default";
+        ResultSet rs = null;
+        try {
+            DbConnector db = new DbConnector();
+            Connection con = db.getCon();
+
+            String rawString = "select name from school where school_id = ? ";
+            PreparedStatement statement = con.prepareStatement(rawString);
+            statement.setString(1, school_id);
+            rs = statement.executeQuery();
+
+        } catch (SQLException ex) {
+
+        }
+
+        if(rs.next()) {
+            name = rs.getString(1);
+        }
+        return name;
+    }
+    
+    public String getUserSchool(String user_name) throws IOException, SQLException {
+        String  school_id = "default";
+        ResultSet rs = null;
+        try {
+            DbConnector db = new DbConnector();
+            Connection con = db.getCon();
+
+            String rawString = "select school_id from user_school natural join user where user_name = ? ";
+            PreparedStatement statement = con.prepareStatement(rawString);
+            statement.setString(1, user_name);
+            rs = statement.executeQuery();
+
+        } catch (SQLException ex) {
+
+        }
+
+        if(rs.next()) {
+            school_id = rs.getString(1);
+        }
+        return school_id;
+    }
 
     public ArrayList<String> getRefereeDetails(String student_ID) throws IOException, SQLException {
         ArrayList<String> details = new ArrayList<>();
@@ -112,14 +156,13 @@ public class Interview {
         try {
             DbConnector db = new DbConnector();
             Connection con = db.getCon();
-            
+
             String rawString = "select distinct first_name, last_name,grade_5,ol,al,other from student, student_school where student.student_id = ? and student_school.student_id = ? and student.student_id = student_school.student_id";
             PreparedStatement statement = con.prepareStatement(rawString);
-            statement.setString(1,student_ID);
-            statement.setString(2,student_ID);
+            statement.setString(1, student_ID);
+            statement.setString(2, student_ID);
             rs = statement.executeQuery();
-            
-            
+
         } catch (SQLException ex) {
             details.add("error occured");
         }
@@ -137,7 +180,6 @@ public class Interview {
         try {
             DbConnector db = new DbConnector();
             Connection con = db.getCon();
-            
 
             Date date = new Date();
             int dd = date.getDate();
@@ -147,17 +189,46 @@ public class Interview {
 
             String rawString = "insert into interview values(?,?,?,?,?)";
             PreparedStatement statement = con.prepareStatement(rawString);
-            statement.setString(1,u_id);
-            statement.setString(2,app_id);
-            statement.setString(3,school_id);
+            statement.setString(1, u_id);
+            statement.setString(2, app_id);
+            statement.setString(3, school_id);
             String mk = mark + "";
-            statement.setString(4,mk);
-            statement.setString(5,dt);
+            statement.setString(4, mk);
+            statement.setString(5, dt);
             statement.executeUpdate();
-            
+
         } catch (SQLException ex) {
             return "error";
         }
         return "success";
+    }
+
+    public ArrayList<ArrayList<String>> getResults(String school_id) throws IOException, SQLException {
+        ArrayList<ArrayList<String>> resultSet = new ArrayList<>();
+        ResultSet rs = null;
+        try {
+            DbConnector db = new DbConnector();
+            Connection con = db.getCon();
+
+            String rawString = "select applicant_id, first_name, last_name, initials, interview_marks from applicant natural join interview where school_id = ? order by interview_marks DESC";
+            PreparedStatement statement = con.prepareStatement(rawString);
+            statement.setString(1, school_id);
+            rs = statement.executeQuery();
+
+        } catch (SQLException ex) {
+            
+        }
+        int rank = 1;
+        while (rs.next()) {
+            ArrayList<String> newRow = new ArrayList<String>();
+            //[ID, full name, marks, rank]
+            newRow.add(rs.getString(1));
+            newRow.add((rs.getString(4) + " " + rs.getString(2) + " " + rs.getString(3)));
+            newRow.add(rs.getString(5));
+            newRow.add((rank + ""));
+            rank++;
+            resultSet.add(newRow);
+        }
+        return resultSet;
     }
 }
